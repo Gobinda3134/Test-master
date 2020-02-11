c2d213c66dd2f35d6b99da4c56d6e76a194aeb6a
Gobinda3134 <bprajwol10@gmail.com>
2020-02-11 10:22:57 -0500

    some changing

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f08002a;
         public static final int end = 0x7f080055;
-        public static final int gone = 0x7f08006b;
-        public static final int invisible = 0x7f08007f;
-        public static final int left = 0x7f080084;
-        public static final int packed = 0x7f0800b3;
-        public static final int parent = 0x7f0800b5;
-        public static final int percent = 0x7f0800b8;
-        public static final int right = 0x7f0800c9;
-        public static final int spread = 0x7f0800ef;
-        public static final int spread_inside = 0x7f0800f0;
-        public static final int start = 0x7f0800f5;
-        public static final int top = 0x7f080111;
-        public static final int wrap = 0x7f08012d;
+        public static final int gone = 0x7f08006c;
+        public static final int invisible = 0x7f080081;
+        public static final int left = 0x7f080086;
+        public static final int packed = 0x7f0800b5;
+        public static final int parent = 0x7f0800b8;
+        public static final int percent = 0x7f0800bb;
+        public static final int right = 0x7f0800cc;
+        public static final int spread = 0x7f0800f2;
+        public static final int spread_inside = 0x7f0800f3;
+        public static final int start = 0x7f0800f8;
+        public static final int top = 0x7f080114;
+        public static final int wrap = 0x7f080130;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/e_softwarica/Fingerprints.java
+++ app/src/main/java/com/example/e_softwarica/Fingerprints.java
@@ -51,12 +51,7 @@ public class Fingerprints extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingerprints);
 
-        fingerprintImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(Fingerprints.this, LoginActivity.class);
-                startActivity(intent);            }
-        });
+      
 
 
         mHeadingLabel = (TextView) findViewById(R.id.headingLabel);
